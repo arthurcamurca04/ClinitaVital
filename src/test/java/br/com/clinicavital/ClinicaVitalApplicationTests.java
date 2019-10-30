a9ac0d0063b6a35f413259dc8b4b4598514b15a8
Arthur <arthur.04.pb@gmail.com>
2019-10-30 08:08:16 -0300

    primeiro commit do projeto Clínica Vital

---
+++ src/test/java/br/com/clinicavital/ClinicaVitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clinicavital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaVitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
