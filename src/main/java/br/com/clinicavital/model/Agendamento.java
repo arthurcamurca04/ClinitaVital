dfd7e46fa28d05c67e19d3081e690f57f8cfc758
Vinicius <minervinocva@gmail.com>
2019-10-30 10:23:04 -0300

    Classes EspecialidadesFisioterapeuta e EspecialidadesNutricionistta

--- src/main/java/br/com/clinicavital/model/Agendamento.java
+++ src/main/java/br/com/clinicavital/model/Agendamento.java
@@ -2,14 +2,12 @@ package br.com.clinicavital.model;
 
 import java.time.LocalDateTime;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.validation.constraints.NotBlank;
 
 import org.springframework.format.annotation.DateTimeFormat;
