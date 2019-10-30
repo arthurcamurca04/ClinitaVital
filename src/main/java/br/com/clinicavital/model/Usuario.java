97d86e918a22f40a2d63a81a1dbd510aee0286fb
Vinicius <minervinocva@gmail.com>
2019-10-30 08:52:44 -0300

    Classe Usuario

--- src/main/java/br/com/clinicavital/model/Usuario.java
+++ src/main/java/br/com/clinicavital/model/Usuario.java
@@ -1,5 +1,61 @@
 package br.com.clinicavital.model;
 
-public class Usuario {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
+@Entity
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	@NotBlank(message = "Insira seu nome")
+	private String nome;
+	@Column
+	@NotBlank(message = "Insira seu e-mail")
+	private String email;
+	@Column
+	@NotBlank(message = "Insira sua senha")
+	private String senha;
+	@Column
+	private boolean ativo;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	
 }
