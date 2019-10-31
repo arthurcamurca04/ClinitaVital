e3f8d1c15a4c0b25dfaa7c5413d0bdd3353b418e
Arthur <arthur.04.pb@gmail.com>
2019-10-31 07:09:10 -0300

    testes unidade de paciente

--- src/test/java/br/com/clinicavital/ClinicaVitalApplicationTests.java
+++ src/test/java/br/com/clinicavital/ClinicaVitalApplicationTests.java
@@ -1,13 +1,61 @@
 package br.com.clinicavital;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.clinicavital.model.Endereco;
+import br.com.clinicavital.model.Paciente;
+import br.com.clinicavital.model.Usuario;
+import br.com.clinicavital.repositry.PacienteReporitory;
 
 @SpringBootTest
 class ClinicaVitalApplicationTests {
 
+	@Autowired
+	private PacienteReporitory rep;
+	
 	@Test
 	void contextLoads() {
+		
 	}
-
+	
+	@Test
+	public void inserirPaciente() {
+		Usuario user = new Usuario();
+		Paciente paciente = new Paciente();
+		Endereco end = new Endereco();
+		
+		user.setNome("André");
+		user.setAtivo(true);
+		user.setEmail("andre@test.com");
+		user.setSenha("1234");
+		
+		end.setBairro("Salgadiho");
+		end.setCep("58796000");
+		end.setCidade("Patos");
+		end.setEstado("PB");
+		end.setRua("Rua projetada");
+		
+		paciente.setUsuario(user);
+		paciente.setCpf("12345678911");
+		paciente.setTelefone("933335566");
+		paciente.setEndereco(end);
+		
+		rep.save(paciente);
+	}
+	
+	//@Test
+	public void alterarPaciente() {
+		Paciente paciente = rep.findById(3L).get();
+		paciente.getUsuario().setNome("Carlos Reinaldo");
+		paciente.getEndereco().setCidade("Piancó");
+		rep.save(paciente);
+	}
+	
+	//@Test
+	public void excluir() {
+		Paciente paciente = rep.findById(3L).get();
+		rep.delete(paciente);
+	}	
 }
