f3a68c3b7899a05cb7b4bf3ea52cbd67d9f4ce60
Arthur <arthur.04.pb@gmail.com>
2019-11-27 11:16:27 -0300

    bootstrap da página cadastro paciente

--- src/test/java/br/com/clinicavital/AdminTest.java
+++ src/test/java/br/com/clinicavital/AdminTest.java
@@ -2,7 +2,7 @@ package br.com.clinicavital;
 
 import java.util.List;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
